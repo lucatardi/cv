199ec4c3852fd962c29aa25fc71e68ec42066d8f
Luca tardito <lucatardi@gmail.com>
2020-01-26 13:03:50 +0000

    first set up

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     html2canvas(document.body, {backgroundColor: '#ffffff'}).then(function(canvas) {
+//         document.body.appendChild(canvas);
+//     });
+// })
